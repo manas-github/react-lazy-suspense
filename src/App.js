422c957fac7c200bef10e0931f5f6cf97f84bcb4
manas <manas.rishav@gmail.com>
2019-11-23 15:08:54 +0530

    simple lazy loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Vegetables = React.lazy(() => import('./Vegetables'));
+const Fruits = React.lazy(() => import('./Fruits'));
+const Snacks = React.lazy(() => import('./Snacks'));
+const Drinks = React.lazy(() => import('./Drinks'));
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 };
+    };
+    updateCount = (count) => {
+        this.setState({count:count})
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>SHOP HERE</h1>
+                <Suspense fallback={<h1>Still Loading…</h1>}>
+                    <h1 onClick={() => this.updateCount(0)}>Veggies</h1>
+                    {this.state.count == 0 && <Vegetables />}
+                    <h1 onClick={() => this.updateCount(1)}>Fruits</h1>
+                    {this.state.count == 1 && <Fruits />}
+                    <h1 onClick={() => this.updateCount(2)}>Snacks</h1>
+                    {this.state.count == 2 && <Snacks />}
+                    <h1 onClick={() => this.updateCount(3)}>Drinks</h1>
+                    {this.state.count == 3 && <Drinks />}
+                </Suspense>
+            </div>
+        );
+    }
 }
 
-export default App;
